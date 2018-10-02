8f398fd83083b9dfa0dc3944c94bbcff8a9fe923
Lqlsoftware <lqlsoft@hotmail.com>
2018-10-02 14:15:46 +0800

    refactor cache to buffer

--- src/io/interface.go
+++ src/io/interface.go
@@ -1,6 +1,11 @@
 package io
 
+// Address contains: file idx, file Offset
+type Address 	uint32
+// Size is the value length in file
+type Size		uint32
+
 type IO interface {
-	Write()
-	Read()
+	Write(data []byte) (address Address, size Size)
+	Read(address Address, size Size) (data []byte)
 }
