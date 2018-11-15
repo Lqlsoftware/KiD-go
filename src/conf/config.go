6609d5f3c4919467f1c93fda8861529f4605178c
Lqlsoftware <lqlsoft@hotmail.com>
2018-11-15 11:35:34 +0800

    new config

--- src/conf/config.go
+++ src/conf/config.go
@@ -3,4 +3,13 @@ package conf
 type KiDConfig struct {
 	CMAP_BLOCK_NUM 			uint8
 	CMAP_BLOCK_INIT_SIZE 	uint32
+
+	BUFFER_MAP_INIT_SIZE 	uint32
+}
+
+var DefaultConfig = &KiDConfig {
+	CMAP_BLOCK_NUM:			16,
+	CMAP_BLOCK_INIT_SIZE:	1024,
+
+	BUFFER_MAP_INIT_SIZE:	1024,
 }
